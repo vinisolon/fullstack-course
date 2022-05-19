5518eadd75d1e492eaf562bcf5866f7aaf3240a7
Vinicius Solon <vini.97@live.com>
2022-05-19 12:35:15 -0300

    Configuração DB H2 e criação automática da entidade Categoria na base

--- src/main/java/com/vinisolon/fullstackcourse/domain/Categoria.java
+++ src/main/java/com/vinisolon/fullstackcourse/domain/Categoria.java
@@ -6,11 +6,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.io.Serializable;
 
+@Entity
 @Getter
 @Setter
 @AllArgsConstructor
