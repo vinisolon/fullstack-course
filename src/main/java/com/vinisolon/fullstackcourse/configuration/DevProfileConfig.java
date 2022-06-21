116961f6fc100d550210783a22d79b4b0e155900
Vinicius Solon <vini.97@live.com>
2022-06-21 19:56:52 -0300

    Criado perfil de desenvolvimento e adicionado dependencia do mysql connector

---
+++ src/main/java/com/vinisolon/fullstackcourse/configuration/DevProfileConfig.java
@@ -0,0 +1,31 @@
+package com.vinisolon.fullstackcourse.configuration;
+
+import com.vinisolon.fullstackcourse.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("dev")
+public class DevProfileConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String dbGenerationStrategy;
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+        if (!dbGenerationStrategy.equals("create"))
+            return false;
+
+        dbService.getTestProfileDatabaseInstance();
+        return true;
+    }
+
+}
